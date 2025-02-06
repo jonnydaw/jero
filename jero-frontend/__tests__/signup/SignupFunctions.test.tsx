078e5777199a7a49bf7f4b70f4de7085daecee41
jonnydaw <jonathandaw29@gmail.com>
2025-02-06 16:34:51 +0000

    email unit tests for helper methods https://trello.com/c/2RUyjmFP

---
+++ jero-frontend/__tests__/signup/SignupFunctions.test.tsx
@@ -0,0 +1,34 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import { isValidEmail } from '@/app/components/Signup/SignupErrors'
+
+// Email Test Start
+
+test('Valid Email Test: Expect False (no @)', () => {
+    const res = isValidEmail("testemail.com")
+    expect(res).toBe(false);
+})
+
+test('Valid Email Test: Expect False (no domain)', () => {
+    const res = isValidEmail("testemail@email")
+    expect(res).toBe(false);
+})
+
+test('Valid Email: Expect False (contains space)', () => {
+    const res = isValidEmail("teste mail@email")
+    expect(res).toBe(false);
+})
+
+test('Valid Email: Expect True (Valid email)', () => {
+    const res = isValidEmail("testEmail@email.com")
+    expect(res).toBe(true);
+})
+
+test('Valid Email: Expect True (Can contain accent)', () => {
+    const res = isValidEmail("testémail@email.com")
+    expect(res).toBe(true);
+})
+
+// Email Test End
+
+
