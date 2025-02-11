56e40ba373a3e35a3e0c5c6781f0c79cce61ffbc
jonnydaw <jonathandaw29@gmail.com>
2025-02-11 23:26:39 +0000

    refactor sign up

--- jero-frontend/app/components/Signup/Signup.tsx
+++ jero-frontend/app/components/Signup/Signup.tsx
@@ -23,27 +23,29 @@ type FormData = {
     roles : string
 }
 
+
+type EmailOrPasswordError =  {
+    empty : boolean,
+    invalid : boolean,
+    mismatch : boolean
+    blurred : boolean
+}
+
+type GeneralErrors = {
+    empty : boolean,
+    blurred : boolean 
+}
+
+
 type FormErrors = {
     firstName : boolean,
     lastName : boolean,
-    email : boolean,
-    confirmEmail : boolean,
+    email : EmailOrPasswordError,
+    confirmEmail : EmailOrPasswordError,
     dob: boolean,
-    password : boolean,
-    confirmPassword : boolean,
+    password : EmailOrPasswordError,
+    confirmPassword : EmailOrPasswordError,
     roles : boolean
-}
-
-type EmailError = {
-    empty : boolean,
-    invalid : boolean,
-    mismatch : boolean
-}
-
-type ConfirmEmailError = {
-    empty : boolean,
-    invalid : boolean,
-    mismatch : boolean
 }
 
 
@@ -59,33 +61,6 @@ const Signup = () => {
         return now.valueOf() - dob.valueOf();
     }
 
-    const [firstNameError, setFirstNameError] = useState<boolean>(false);
-    const [lastNameError, setLastNameError] = useState<boolean>(false);
-    
-    const [emailError, setEmailError] = useState<EmailError>({
-        empty : false,
-        invalid : false,
-        mismatch: false
-    });
-
-    const [confirmEmailError, setConfirmEmailError] = useState<ConfirmEmailError>({
-        empty : false,
-        invalid : false,
-        mismatch: false
-    });
-
-    const [passwordError, setPasswordError] = useState<ConfirmEmailError>({
-        empty : false,
-        invalid : false,
-        mismatch: false
-    });
-
-    const [confirmPasswordError, setConfirmPasswordError] = useState<ConfirmEmailError>({
-        empty : false,
-        invalid : false,
-        mismatch: false
-    });
-
     const [submissionError, setSubmissionError] = useState<boolean>(false);
     
     const [formData, setFormData] = useState<FormData>({
@@ -99,157 +74,89 @@ const Signup = () => {
         roles : ""
     });
 
-
-    // true signifies the lack of an error.
-    // having to use useRef as useState was not fast enough on its own
     const [errors, setErrors] = useState<FormErrors>({
         firstName : false,
         lastName : false,
-        email : false,
-        confirmEmail : false,
+        email : {empty: false, invalid: false,mismatch: false, blurred : false},
+        confirmEmail : {empty: false, invalid: false,mismatch: false, blurred : false},
         dob: false,
-        password : false,
-        confirmPassword : false,
+        password : {empty: false, invalid: false,mismatch: false, blurred : false},
+        confirmPassword : {empty: false, invalid: false,mismatch: false, blurred : false},
         roles : false
     })
+// could be tidier still
+    const handleValidationPerInput = (name: string, value: string, blurredStatus : boolean) => {
+        if(name === `firstName`){
+            setErrors({...errors, [name] : value.length === 0 })
+        } else if(name === `lastName`){
+            setErrors({...errors, [name] : value.length === 0})
+        } else if(name === `email`){
+            if(blurredStatus || errors.email.blurred === true){
+            setErrors({...errors, [name] : 
+                {
+                empty: value.length === 0,
+                invalid : !isValidEmail(value),
+                mismatch: !isFieldMatch(formData.confirmEmail, value),
+                blurred : true
+                }
+        })
+    }
+        } else if(name === `confirmEmail`){
+            if(blurredStatus || errors.confirmEmail.blurred === true){
+            setErrors({...errors, [name] : 
+                {
+                empty: value.length === 0,
+                invalid : !isValidEmail(value),
+                mismatch : !isFieldMatch(formData.email, value),
+                blurred : true
+                }
+        })
+    }
 
-    // TODO: REFACTOR
+        } else if(name === `dob`){
+            console.log(calculateAge());
+        } else if (name === `password`){
+            if(blurredStatus || errors.password.blurred === true)
+            setErrors({...errors, [name] : 
+                {
+                empty: value.length === 0,
+                invalid : !isValidPassword(value),
+                mismatch: !isFieldMatch(formData.confirmPassword, value),
+                blurred : true
+            }
+        })
+        } else if(name === `confirmPassword`){
+            console.log(isValidPassword(value));
+            if(blurredStatus || errors.confirmPassword.blurred === true){
+            setErrors({...errors, [name] : 
+                {
+                empty: value.length === 0,
+                invalid : !isValidPassword(value),
+                mismatch: !isFieldMatch(formData.password, value),
+                blurred : true
+                }
+        })
+        }
+    }
+        
+    }
     const handleChange = (e : any) => {
         const { name, value} = e.target;
         setFormData({ ...formData, [name]: value });
-        if(name === `firstName` && value.length > 0 ){
-            setFirstNameError(false);
-        } else if(name === `lastName` && value.length > 0){
-            setLastNameError(false);
-        } else if(name === `email` && value.length > 0){
-            setEmailError({...emailError, [`empty`] : false});
-            if(isValidEmail(value)){
-                setEmailError({...emailError, [`invalid`] : false});
-            }
-            if(isFieldMatch(formData.confirmEmail, value)){
-                setConfirmEmailError({...confirmEmailError, [`mismatch`] : false});
-            }
-        } else if(name === `confirmEmail` && value.length > 0){
-            setConfirmEmailError({...confirmEmailError, [`empty`] : false});
-            if(isValidEmail(value)){
-                setConfirmEmailError({...confirmEmailError, [`invalid`] : false});
-            }
-            if(isFieldMatch(formData.email, value)){
-                setConfirmEmailError({...confirmEmailError, [`mismatch`] : false});
-            }
-        } else if(name === `dob`){
-            console.log(calculateAge());
-        } else if (name === `password` && value.length > 0){
-            setPasswordError({...passwordError, [`empty`] : false});
-            if(isValidPassword(value)){
-                setPasswordError({...passwordError, [`invalid`] : false});
-            }
-            if(isFieldMatch(formData.confirmPassword, value)){
-                setConfirmPasswordError({...confirmPasswordError, [`mismatch`] : false});
-            }
-        } else if(name === `confirmPassword` && value.length > 0){
-            console.log(isValidPassword(value));
-            setConfirmPasswordError({...confirmPasswordError, [`empty`] : false});
-            if(isValidPassword(value)){
-                console.log("valid")
-                setConfirmPasswordError({...confirmPasswordError, [`invalid`] : false});
-            }
-            if(isFieldMatch(formData.password, value)){
-                setConfirmPasswordError({...confirmPasswordError, [`mismatch`] : false});
-            }
-        }
+        handleValidationPerInput(name,value, false);
+       
     }
 
-    const handleFocus = (e : any) =>{
-
-    }
-
-    const handleBlurFirstName = () => {
-        if(formData.firstName.length === 0){
-            setFirstNameError(true)
-        }else{
-            setFirstNameError(false)
-        }
-    }
-
-    const handleBlurLastName = () => {
-        if(formData.lastName.length === 0){
-            setLastNameError(true)
-        }else{
-            setLastNameError(false)
-        }
-    }
-
-    const handleBlurEmail = () => {
-        if(formData.email.length === 0){
-            setEmailError({...emailError, [`empty`] : true})
-        }else if(!isValidEmail(formData.email)) {
-            setEmailError({...emailError, [`invalid`] : true})
-        } else if (formData.confirmEmail .length > 0 && !isFieldMatch(formData.email, formData.confirmEmail)){
-            setConfirmEmailError({...confirmEmailError, [`mismatch`] : true})
-        }
-    }
-
-    const handleBlurConfirmEmail = () => {
-        if(formData.confirmEmail.length === 0){
-            setConfirmEmailError({...confirmEmailError, [`empty`] : true})
-        }else if(!isValidEmail(formData.confirmEmail)) {
-            setConfirmEmailError({...confirmEmailError, [`invalid`] : true})
-        } else if (!isFieldMatch(formData.email, formData.confirmEmail)){
-            setConfirmEmailError({...confirmEmailError, [`mismatch`] : true})
-        }
+    const handleBlur = (e : any) => {
+        const { name, value} = e.target;
+        handleValidationPerInput(name,value, true);
     }
 
 
-    const handleBlurPassword = () => {
-        if(formData.password.length === 0){
-            setPasswordError({...passwordError, [`empty`] : true})
-        }else if(!isValidPassword(formData.password)) {
-            setPasswordError({...passwordError, [`invalid`] : true})
-        } else if (formData.confirmPassword .length > 0 && !isFieldMatch(formData.password, formData.confirmPassword)){
-            setConfirmPasswordError({...confirmPasswordError, [`mismatch`] : true})
-        }
-    }
-
-    const handleBlurConfirmPassword = () => {
-        if(formData.confirmPassword.length === 0){
-            setConfirmPasswordError({...confirmPasswordError, [`empty`] : true})
-        }else if(!isValidPassword(formData.confirmPassword)) {
-            setConfirmPasswordError({...confirmPasswordError, [`invalid`] : true})
-        } else if (!isFieldMatch(formData.password, formData.confirmPassword)){
-            setConfirmPasswordError({...confirmPasswordError, [`mismatch`] : true})
-        }
-    }
 
 
     const allowSubmission = () : boolean => {
-        if(firstNameError){
-            console.log("fname")
-            return false;
-        }
-        else if(lastNameError){
-            console.log("lname");
-            return false;
-        }
-        else if(emailError.empty || emailError.invalid || emailError.mismatch){
-            console.log("e")
-            return false;
-        }
-        else if(confirmEmailError.empty || confirmEmailError.invalid || confirmEmailError.mismatch){
-            console.log("ce")
-            return false;}
-        else if(passwordError.empty || passwordError.invalid || passwordError.mismatch){
-            console.log("p")
-            return false;
-        }
-        else if(confirmPasswordError.empty || confirmPasswordError.invalid || confirmPasswordError.mismatch){
-            console.log("confirmP")
-            return false;
-        }
-        else if(formData.roles === t('userTypes.tourist') || formData.roles === t('userTypes.host') || formData.roles === (t('userTypes.both'))){
-            console.log("user")
-            return false;
-        }
+        // to do
         setSubmissionError(false);
         console.log("no errors")
         return true;
@@ -257,7 +164,7 @@ const Signup = () => {
 
     const handleSubmit = async (e : any) => {
         e.preventDefault();
-        console.log(emailError)
+        //console.log(emailError)
         if(allowSubmission()){
             setPostSuccess(true)
         try {
@@ -289,7 +196,7 @@ const Signup = () => {
         <div>
         <form onSubmit={handleSubmit} id={style.form}>
 
-            {!firstNameError ? <br /> : <span>First name cannot be empty.</span>}
+            {!errors.firstName ? <br /> : <span>First name cannot be empty.</span>}
             <input
                 type="text"
                 id="firstName"
@@ -297,10 +204,10 @@ const Signup = () => {
                 placeholder= {t('firstName')}
                 value={formData.firstName}
                 onChange={handleChange}
-                onBlur={handleBlurFirstName}
+                onBlur={handleBlur}
             />
 
-            {!lastNameError ? <br /> : <span>Last name cannot be empty.</span>}
+            {!errors.lastName ? <br /> : <span>Last name cannot be empty.</span>}
             <input
                 type="text"
                 id="lastName"
@@ -308,10 +215,10 @@ const Signup = () => {
                 placeholder= {t('lastName')}
                 value={formData.lastName}
                 onChange={handleChange}
-                onBlur={handleBlurLastName}
+                onBlur={handleBlur}
             />
 
-            {!firstNameError ? <span>Date of Birth</span> : <span>Not allowed</span>}
+            {!errors.firstName ? <span>Date of Birth</span> : <span>Not allowed</span>}
             <input
                 type="date"
                 id="dob"
@@ -319,7 +226,7 @@ const Signup = () => {
                 value={formData.dob}
                 onChange={handleChange}
             />
-            {(!emailError.empty && !emailError.invalid) ? <br /> : <span>{emailError.empty ? `Email cannot be empty` : `Invalid Email`}</span> }
+            {(!errors.email.empty && !errors.email.invalid) ? <br /> : <span>{errors.email.empty ? `Email cannot be empty` : `Invalid Email`}</span> }
             <input
                 type="email"
                 id="email"
@@ -327,14 +234,14 @@ const Signup = () => {
                 placeholder= {t('email')}
                 value={formData.email}
                 onChange={handleChange}
-                onBlur={handleBlurEmail}
+                onBlur={handleBlur}
 
             />
-            {(!confirmEmailError.empty && !confirmEmailError.invalid && !confirmEmailError.mismatch)  
+            {(!errors.confirmEmail.empty && !errors.confirmEmail.invalid && !errors.confirmEmail.mismatch)  
             ? <br /> 
             : <span>
-                {confirmEmailError.empty ? `Email cannot be empty` : 
-                confirmEmailError.invalid ? `Email is invalid`: `Email mismatch`}
+                {errors.confirmEmail.empty ? `Email cannot be empty` : 
+                errors.confirmEmail.invalid ? `Email is invalid`: `Email mismatch`}
             </span>}
             <input
                 type="email"
@@ -343,9 +250,9 @@ const Signup = () => {
                 placeholder= {t('confirmEmail')}
                 value={formData.confirmEmail}
                 onChange={handleChange}
-                onBlur={handleBlurConfirmEmail}
+                onBlur={handleBlur}
             />
-            {(!passwordError.empty && !passwordError.invalid) ? <br /> : <span>{passwordError.empty ? `Password cannot be empty` : `Invalid Password`}</span> }
+            {(!errors.password.empty && !errors.password.invalid) ? <br /> : <span>{errors.password.empty ? `Password cannot be empty` : `Invalid Password`}</span> }
             <input
                 type="password"
                 id="password"
@@ -353,13 +260,13 @@ const Signup = () => {
                 placeholder= {t('password')}
                 value={formData.password}
                 onChange={handleChange}
-                onBlur={handleBlurPassword}
+                onBlur={handleBlur}
             />
-            {(!confirmPasswordError.empty && !confirmPasswordError.invalid && !confirmPasswordError.mismatch)  
+            {(!errors.confirmPassword.empty && !errors.confirmPassword.invalid && !errors.confirmPassword.mismatch)  
             ? <br /> 
             : <span>
-                {confirmPasswordError.empty ? `Password cannot be empty` : 
-                confirmPasswordError.invalid ? `Password is invalid`: `Password mismatch`}
+                {errors.confirmPassword.empty ? `Password cannot be empty` : 
+                errors.confirmPassword.invalid ? `Password is invalid`: `Password mismatch`}
             </span>}
             <input
                 type="password"
@@ -368,7 +275,7 @@ const Signup = () => {
                 placeholder= {t('confirmPassword')}
                 value={formData.confirmPassword}
                 onChange={handleChange}
-                onBlur={handleBlurConfirmPassword}
+                onBlur={handleBlur}
             />
             {true ? <br /> : <span>An option must be selected</span>}
             <select name="roles"
