7e1d2869ec9fa7eaf7d940d821862defade8c935
jonnydaw <jonathandaw29@gmail.com>
2025-02-24 14:16:38 +0000

    will this work 2

--- jero-frontend/app/[locale]/property/[property_id]/page.tsx
+++ jero-frontend/app/[locale]/property/[property_id]/page.tsx
@@ -1,6 +1,6 @@
 
  // https://stackoverflow.com/questions/77412027/using-next-13-5-6-app-router-how-to-get-params-of-dynamic-route
-const page = async ({ params }: { params: { property_id: string} }) => {
+const page = async ({params}: {params: Promise<{ property_id: string }>}) => {
 
     const { property_id } = await params
     return <p>ID: {property_id}</p>
