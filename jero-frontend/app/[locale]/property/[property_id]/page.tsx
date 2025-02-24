33286a9f92600068af728543a56cfb638fa9f05d
jonnydaw <jonathandaw29@gmail.com>
2025-02-24 13:55:26 +0000

    proper export

--- jero-frontend/app/[locale]/property/[property_id]/page.tsx
+++ jero-frontend/app/[locale]/property/[property_id]/page.tsx
@@ -1,6 +1,6 @@
 
  // https://stackoverflow.com/questions/77412027/using-next-13-5-6-app-router-how-to-get-params-of-dynamic-route
-export const page = ({ params }: { params: { property_id: string } }) => {
+const page = ({ params }: { params: { property_id: string } }) => {
 
   return <p>Post: {params?.property_id}</p>
 }
