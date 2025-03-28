9456e22317f6b43b9a743a12df4e0cd8a61d7aaa
jonnydaw <jonathandaw29@gmail.com>
2025-03-28 22:25:31 +0000

    this better work

--- jero-frontend/components/AdvancedSearch/AdvancedSearchPC/AdvancedSearchPC.tsx
+++ jero-frontend/components/AdvancedSearch/AdvancedSearchPC/AdvancedSearchPC.tsx
@@ -4,13 +4,59 @@ import style from "./AdvancedSearchPC.module.css"
 import GuestToggler from '../../Navbar/NavbarPC/GuestDropdown/GuestToggler'
 import { useState } from 'react'
 import { GuestCounts } from '@/app/types/types'
+import MultiRangeSlider from "multi-range-slider-react";
+import { SiBitly } from 'react-icons/si'
+
+
+interface FormData {
+    start : string;
+    end : string;
+    count : GuestCounts;
+    attractions : string[];
+    holidayType : string;
+    guestTypes : string;
+    gettingAround : string;
+}
+
 export const AdvancedSearchPC = () => {
+    const [minEndDate, setMinEndDate] = useState<string>("");
+
     const [count, setCount] = useState<GuestCounts>({adultCount : 1, childCount : 0, petCount : 0});
+    const [formData, setFormData] = useState<FormData>(
+        {
+        start: "",
+        end : "",
+        count: {adultCount : 1, childCount : 0, petCount : 0},
+        attractions : [],
+        holidayType : "",
+        guestTypes : "",
+        gettingAround : "",
+    }
+);    
+    
+
+    const handleChange = (e: any) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name] : value });
+        if(name === `start`){
+          let startPlusDay : Date = new Date(value);
+          startPlusDay.setDate(startPlusDay.getDate() + 1);
+          const minEndDate = startPlusDay.toISOString().split("T")[0]; 
+          setMinEndDate(minEndDate);
+        }
+      };
+
+    const [minValue, set_minValue] = useState(18);
+const [maxValue, set_maxValue] = useState(26);
+    const handleInput = (e : any) => {
+        set_minValue(e.minValue);
+        set_maxValue(e.maxValue);
+    };
 
     return(
         <div id={style.container}>
             <div>
-            <Form action="/search">
+            <form>
             <fieldset className={style.fields}>
                 </fieldset>
             <fieldset className={style.fields}>
@@ -33,21 +79,54 @@ export const AdvancedSearchPC = () => {
                 <fieldset  className={style.fields} >
                 <div className={style.underlineDiv}>
                  <h3>Weather at time of travel</h3>
+                 <MultiRangeSlider
+                 id={style.rangeSlider}
+                    style={{border : "none", boxShadow : "none"}}
+                    min={-20}
+                    barInnerColor='black'
+                    max={50}
+                    step={1}
+                    minValue={minValue}
+                    maxValue={maxValue}
+                    ruler={false}
+                    label={true}
+                    onInput={(e) => {
+                        handleInput(e);
+                    }}/>
                 </div>
                 <div className={style.underlineDiv}>
-                 <h3>Daily expenses</h3>
+                 <h3>Attractions</h3>
+                 <input type="checkbox" />
                 </div>
                 <div className={style.underlineDiv}>
                  <h3>Location type</h3>
+                    <div className={style.items}>
+                                         
+                    <label htmlFor="beach">Beach</label>
+                    <input type="radio" id="beach" name="beach" />
+
+                    <label htmlFor="city">City</label>
+                    <input type="radio" id="city" name="city" />
+
+                    <label htmlFor="city">Village</label>
+                    <input type="radio" id="village" name="skiing" />
+
+                    <label htmlFor="city">Skiing</label>
+                    <input type="radio" id="skiing" name="skiing" />
+                 
+                    </div>
                 </div>
                 <div className={style.underlineDiv}>
                  <h3>Tourism Levels</h3>
                 </div>
                 <div className={style.underlineDiv}>
-                 <h3>Family friendly etc</h3>
+                    <h3>Family friendly etc</h3>
+                </div>
+                <div className={style.underlineDiv}>
+                    <h3>Getting around</h3>                
                 </div>
                 </fieldset>
-            </Form>
+            </form>
             </div>
         </div>
     )
