447d5b0c2d842cb482fa8a36c123b6a372d0015d
jonnydaw <jonathandaw29@gmail.com>
2025-04-13 22:23:44 +0100

    logout bug fix

--- jero-backend/demo/demo/src/main/java/com/example/demo/SecurityConfig/ApplicationConfig.java
+++ jero-backend/demo/demo/src/main/java/com/example/demo/SecurityConfig/ApplicationConfig.java
@@ -2,11 +2,14 @@ package com.example.demo.SecurityConfig;
 
 
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.context.annotation.Bean; 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy; 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; 
@@ -41,9 +44,54 @@ public class ApplicationConfig {
 						.requestMatchers("/auth/refresh").permitAll()
 						.anyRequest().permitAll())
 						.logout((logout) -> logout
-							.deleteCookies("JWT","RT")
+							//.deleteCookies("JWT","RT") // local
 							// https://stackoverflow.com/questions/79314702/spring-authorization-server-threw-exception-with-message-delegatingauthenticati
 							.logoutSuccessHandler((request, response, authentication) -> {
+
+								
+								// https://stackoverflow.com/questions/9821919/delete-cookie-from-a-servlet-response
+								// Cookie nullJWTCookie = new Cookie("JWT", null);
+								// nullJWTCookie.setPath("/");
+								// nullJWTCookie.setHttpOnly(true);
+								// nullJWTCookie.setMaxAge(3600);
+								// nullJWTCookie.setSecure(true);
+								// nullJWTCookie.setDomain(".jero.travel");
+
+								
+
+								// Cookie nullRTCookie = new Cookie("RT", null);
+								// nullRTCookie.setPath("/");
+								// nullRTCookie.setHttpOnly(true);
+								// nullRTCookie.setMaxAge(3600);
+								// nullRTCookie.setSecure(true);
+								// nullRTCookie.setDomain(".jero.travel");
+								
+								// response.addCookie(nullJWTCookie);
+								// response.addCookie(nullRTCookie);
+
+								ResponseCookie nullJWTCookie = ResponseCookie.from("JWT", null)
+									.httpOnly(true)
+									.secure(true)
+									.path("/")
+									.maxAge(0) 
+									.sameSite("none")
+									//https://stackoverflow.com/questions/58191969/how-to-fix-set-samesite-cookie-to-none-warning 
+									.domain(".jero.travel")
+									.build();
+								
+								ResponseCookie nullRTCookie = ResponseCookie.from("RT", null)
+									.httpOnly(true)
+									.secure(true)
+									.path("/")
+									.maxAge(0) 
+									.sameSite("none")
+									//https://stackoverflow.com/questions/58191969/how-to-fix-set-samesite-cookie-to-none-warning 
+									.domain(".jero.travel")
+									.build();
+								
+
+								response.addHeader(HttpHeaders.SET_COOKIE, nullJWTCookie.toString());
+								response.addHeader(HttpHeaders.SET_COOKIE, nullRTCookie.toString());
 								response.setStatus(HttpServletResponse.SC_OK);
 								response.getWriter().flush();
 							})
@@ -57,14 +105,13 @@ public class ApplicationConfig {
 			@Override
 			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) { 
 				CorsConfiguration ccfg = new CorsConfiguration(); 
-				ccfg.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://jero.travel", "https://www.jero.travel")); 
+				ccfg.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://jero.travel", "https://www.jero.travel", "*.jero.travel")); 
 				ccfg.setAllowedMethods(Collections.singletonList("*")); 
 				ccfg.setAllowCredentials(true); 
 				ccfg.setAllowedHeaders(Collections.singletonList("*")); 
 				ccfg.setExposedHeaders(Arrays.asList("Authorization")); 
-				ccfg.setMaxAge(3600L); 
+				ccfg.setMaxAge(3600L);
 				return ccfg; 
-
 			} 
 		}; 
 
