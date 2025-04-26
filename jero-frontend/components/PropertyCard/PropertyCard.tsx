deec096cb84d2d7bbb8473d03c7ec76ed5744a82
jonnydaw <jonathandaw29@gmail.com>
2025-04-26 21:11:54 +0100

    property card pricing fix

--- jero-frontend/components/PropertyCard/PropertyCard.tsx
+++ jero-frontend/components/PropertyCard/PropertyCard.tsx
@@ -20,15 +20,17 @@ const PropertyCard = (props : Props) => {
     const childCount = searchParams.get("numchildren");
     const petCount = searchParams.get("numpets");
     const guestTotal = Number(childCount) + Number(adultCount);
-    console.log(Number(props.propertyAttribute.extraGuestPriceIncrease));
     let bookingLength : number | null = null
     if(startDate && endDate){
         bookingLength = ((new Date(endDate || "").getTime() - new Date(startDate || "").getTime()) / 86_400_000);
+    }else{
+        bookingLength = 1;
     }
 
     const t = useTranslations('PropertyCard');
-
-
+console.log("total")
+const basePrice =  Number(props.propertyAttribute.pricePerNight) * bookingLength;
+const extraGuestsCosts = ((guestTotal-1) * Number(props.propertyAttribute.extraGuestPriceIncrease)) * bookingLength;
 
     const handleClick = () =>{
         const params = new URLSearchParams();
@@ -62,7 +64,7 @@ const PropertyCard = (props : Props) => {
             >
                 <h3>{props.propertyAttribute.title.length > 0 ? props.propertyAttribute.title : t('noTitle') }</h3>
                 <img src={props.propertyAttribute.mainImage} alt="Main Property Image" />
-                <strong style={{marginTop : "0.5em"}}>£{Number(props.propertyAttribute.pricePerNight)} per night  {bookingLength && `• £ ${Number(props.propertyAttribute.pricePerNight) * bookingLength} ${t('total')}`}</strong>
+                <strong style={{marginTop : "0.5em"}}>£{Number(props.propertyAttribute.pricePerNight)} per night  {bookingLength && `• £ ${basePrice + extraGuestsCosts} ${t('total')}`}</strong>
                 <em style={{marginBottom : "0.5em"}}>{t('extra')} • £{Number(props.propertyAttribute.extraGuestPriceIncrease)}</em>
                 <p style={{margin : "0em"}}>{props.propertyAttribute.displayLocation}</p>
                 <p>{props.propertyAttribute.percentile > 0 ? `${t('ratedHigher')} ${~~props.propertyAttribute.percentile}%` : t('noReviews') }</p>
